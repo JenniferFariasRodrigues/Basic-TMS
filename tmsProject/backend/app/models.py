1f24e31c8408d88b57c080887b411fa4b60c2033
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-06-05 22:35:28 -0300

    fix: classes and relationship adjustes on the models.py

--- tmsProject/backend/app/models.py
+++ tmsProject/backend/app/models.py
@@ -1,11 +1,13 @@
 from . import db
 
+#agricultural product/item
 class ProduceItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     unit = db.Column(db.String(32), nullable=False)
     category = db.Column(db.String(32), nullable=False)
-
+    
+# many-to-many relationship with ProduceItem code through the carrier_produce_item table
 class Carrier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
@@ -13,8 +15,13 @@ class Carrier(db.Model):
     phone = db.Column(db.String(20), nullable=False)
     company = db.Column(db.String(64), nullable=False)
     address = db.Column(db.String(128), nullable=False)
-    produce_items = db.relationship('ProduceItem', backref='carrier', lazy=True)
-    is_busy = db.Column(db.Boolean, default=False)
+    #relationship beteween ProduceItem and carrier_produce_item
+    produce_items = db.relationship('ProduceItem', secondary='carrier_produce_item', back_populates='carriers')
+
+# many-to-many relationship between Carrier and ProduceItem
+class CarrierProduceItem(db.Model):
+    carrier_id = db.Column(db.Integer, db.ForeignKey('carrier.id'), primary_key=True)
+    produce_item_id = db.Column(db.Integer, db.ForeignKey('produce_item.id'), primary_key=True)
 
 class Load(db.Model):
     id = db.Column(db.Integer, primary_key=True)

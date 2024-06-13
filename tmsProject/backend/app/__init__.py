d560b6d02c9df8e6d5063901f5c1736584a66490
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-06-12 21:46:33 -0300

    fix:cleanning code

--- tmsProject/backend/app/__init__.py
+++ tmsProject/backend/app/__init__.py
@@ -28,18 +28,3 @@ from app import routes, models
 #     db.create_all()
 #     print("Tables created successfully!")
 
-
-# sqlachademy code. Change after
-# from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/tms'
-# db = SQLAlchemy(app)
-
-# from . import routes
-
-# def create_app():
-#     db.create_all()
-#     return app
-

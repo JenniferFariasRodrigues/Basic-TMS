1a467253de5d9c41d87549b5198f7b68e29d967c
JENNIFER FARIAS RODRIGUES <69055449+JenniferFariasRodrigues@users.noreply.github.com>
2024-06-13 05:16:07 -0300

    fix:adjusting  code

--- tmsProject/backend/app/transactional.py
+++ tmsProject/backend/app/transactional.py
@@ -1,20 +1,20 @@
-from functools import wraps
-# from config import db
-from app import db
+# from functools import wraps
+# # from config import db
+# from app import db
 
 
 
-def transactional_session(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        session = db.session
-        try:
-            result = func(*args, **kwargs)
-            session.commit()
-            return result
-        except Exception as e:
-            session.rollback()
-            print(f"Unable to save data in the database: {e}")
-            raise
+# def transactional_session(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         session = db.session
+#         try:
+#             result = func(*args, **kwargs)
+#             session.commit()
+#             return result
+#         except Exception as e:
+#             session.rollback()
+#             print(f"Unable to save data in the database: {e}")
+#             raise
 
-    return wrapper
+#     return wrapper
